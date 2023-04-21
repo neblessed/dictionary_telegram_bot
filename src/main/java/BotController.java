8416b6e72e578f8cc14fa702fff39610a32129e0
Vadim <fw.mbagg@gmail.com>
2023-04-21 20:13:01 +0300

    подключил бота

--- src/main/java/BotController.java
+++ src/main/java/BotController.java
@@ -1,2 +1,44 @@
-public class BotController {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import static config.BotProperties.*;
+
+public class BotController extends TelegramLongPollingBot {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new BotController());
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME ;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Кому сообщение
+                .text(what).build();    //Текст сообщения
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, "TEST");
+    }
 }
