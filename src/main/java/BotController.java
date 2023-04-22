cb316439e8f599d706c972e197dc1ee52621faf3
Vadim <fw.mbagg@gmail.com>
2023-04-22 08:24:13 +0300

    добавил клавиатуру и 3 кнопки в бота

--- src/main/java/BotController.java
+++ src/main/java/BotController.java
@@ -2,8 +2,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static config.BotProperties.*;
 
@@ -15,7 +20,7 @@ public class BotController extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return BOT_NAME ;
+        return BOT_NAME;
     }
 
     @Override
@@ -23,16 +28,36 @@ public class BotController extends TelegramLongPollingBot {
         return BOT_TOKEN;
     }
 
-    public void sendText(Long who, String what){
+    public void sendText(Long who, String what) {
+        // Создаю объект клавиатуры
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        // Создаю список строк с названиями кнопок
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add("Получить слова");
+        keyboard.add(row1);
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add("Количество слов");
+        keyboard.add(row2);
+        KeyboardRow row3 = new KeyboardRow();
+        row3.add("Запустить тест");
+        keyboard.add(row3);
+
+        // Устанавливаю клавиатуру
+        keyboardMarkup.setKeyboard(keyboard);
         SendMessage sm = SendMessage.builder()
                 .chatId(who.toString()) //Кому сообщение
-                .text(what).build();    //Текст сообщения
+                .text(what)
+                .replyMarkup(keyboardMarkup)
+                .build();
         try {
-            execute(sm);                        //Actually sending the message
+            execute(sm);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);      //Any error will be printed here
+            throw new RuntimeException(e);
         }
     }
+
     @Override
     public void onUpdateReceived(Update update) {
         var msg = update.getMessage();
