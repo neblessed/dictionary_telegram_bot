f637cf7e45fd5948e97c990a8d69a38e6419e2ce
Vadim <fw.mbagg@gmail.com>
2023-05-06 09:00:32 +0300

    pom.xml

---
+++ src/main/java/config/BotProperties.java
@@ -0,0 +1,7 @@
+package config;
+
+public class BotProperties {
+    public static final String BOT_TOKEN = "6069364739:AAGcFz_Us1D_1CwWz82WV5slfGWYYxnd3XQ";
+    public static final String BOT_NAME = "ForeignWordOfTheDayBot";
+    public static final String TRANSLATION_API_KEY = "AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw";
+}
